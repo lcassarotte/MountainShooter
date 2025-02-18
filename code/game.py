0f33992a90d7827de8b57335d8c5411b2ce90f90
lcassarotte <leo.cassarotte@gmail.com>
2025-02-18 15:39:55 -0300

    Adding Classes generated from StarUML

--- code/game.py
+++ code/game.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+
+from code.menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        window = pygame.display.set_mode(size=(600, 480))
 
     def run(self, ):
-        pass
+
+        while True:
+            menu = Menu()
+
+
+
+            # Check for all events
+            # for event in pygame.event.get():
+            #   if event.type == pygame.QUIT:
+            #      pygame.quit()  # close window
+            #     quit()  # end pygame
+
